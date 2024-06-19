c699744e2901a87e1bdcd6fc1ffdc08ed71985bd
tk0710 <>
2024-06-20 00:21:59 +0900

    ind

---
+++ htdocs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ゲーム紹介サイト</title>
+</head>
+<body>
+    <h1>ゲーム紹介サイト</h1>
+    <ul>
+        <li><a href="add_game.php">ゲーム追加ページ</a></li>
+        <li><a href="game_list.php">紹介ページ一覧</a></li>
+    </ul>
+</body>
+</html>
