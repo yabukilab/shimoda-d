4dd40ae292e768615a42b93afbec2d1329eb3179
ユーザ名 <>
2024-06-28 15:05:40 +0900

    1505

--- htdocs/edit_error.php
+++ htdocs/edit_error.php
@@ -2,14 +2,18 @@
 <html>
 <head>
     <title>エラー</title>
-    <meta http-equiv="refresh" content="3;url=index.php">
+    <?php
+    // 前ページのURLを取得
+    $previousPage = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
+    ?>
+    <meta http-equiv="refresh" content="3;url=<?php echo htmlspecialchars($previousPage); ?>">
 </head>
 <body>
     <h1>エラーが発生しました</h1>
     <p><?php
         if (isset($_GET['message'])) {
             $message = urldecode($_GET['message']);
-            echo nl2br(htmlspecialchars($message)) . "<br>3秒後にトップページにリダイレクトします。";
+            echo nl2br(htmlspecialchars($message)) . "<br>3秒後に前のページに戻ります。";
         }
     ?></p>
 </body>
