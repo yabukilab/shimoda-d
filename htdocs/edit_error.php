1e7971f4592ebe68e2f6df5969f4d6f5edc8d42f
a <>
2024-06-28 11:30:02 +0900

    エラーのときに三秒後にリダイレクトするようにした

--- htdocs/edit_error.php
+++ htdocs/edit_error.php
@@ -2,13 +2,14 @@
 <html>
 <head>
     <title>エラー</title>
+    <meta http-equiv="refresh" content="3;url=index.php">
 </head>
 <body>
     <h1>エラーが発生しました</h1>
     <p><?php
         if (isset($_GET['message'])) {
             $message = urldecode($_GET['message']);
-            echo nl2br(htmlspecialchars($message));
+            echo nl2br(htmlspecialchars($message)) . "<br>3秒後にトップページにリダイレクトします。";
         }
     ?></p>
     <a href="index.php">トップページに戻る</a>
