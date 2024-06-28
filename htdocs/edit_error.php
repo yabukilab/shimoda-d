ebc322293b7776408e03715016a58f7c7e8fde8a
koshitaku0710 <>
2024-06-28 14:18:47 +0900

    er

--- htdocs/edit_error.php
+++ htdocs/edit_error.php
@@ -2,15 +2,17 @@
 <html>
 <head>
     <title>エラー</title>
-    <meta http-equiv="refresh" content="3;url=index.php">
+    <meta http-equiv="refresh" content="3;url=edit_delete_page.php">
 </head>
 <body>
     <h1>エラーが発生しました</h1>
-    <p><?php
+    <p>
+        <?php
         if (isset($_GET['message'])) {
             $message = urldecode($_GET['message']);
-            echo nl2br(htmlspecialchars($message)) . "<br>3秒後にトップページにリダイレクトします。";
+            echo nl2br(htmlspecialchars($message)) . "<br>3秒後に元のページにリダイレクトします。";
         }
-    ?></p>
+        ?>
+    </p>
 </body>
 </html>
