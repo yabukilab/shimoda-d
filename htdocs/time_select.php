4510eb587e2d6f284ab4f61ada3d102f79df1386
2342044 <s2342044nf@s.chibakoudai.jp>
2025-06-27 15:04:51 +0900

    remove：不要なphpファイルを4つ削除しました（reserved.php,seatselect.php,studentid.php,timeselect.php)
    "add：time_select.phpを追加しました。
    
    時間を選択できるボタン機能を実装しました"

---
+++ htdocs/time_select.php
@@ -0,0 +1,46 @@
+
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>抽選申し込み - 時間帯選択</title>
+    <link rel="stylesheet" href="style.css">
+    </style>
+</head>
+<body>
+
+<h1>抽選申し込み</h1>
+<p>時間帯を選んでください</p>
+
+<div class="time-grid">
+<?php
+$timeSlots = [
+    "11:00-11:29",
+    "11:30-11:59",
+    "12:00-12:29",
+    "12:30-12:59",
+    "13:00-13:29",
+    "13:30-13:59"
+];
+
+foreach ($timeSlots as $slot) {
+    echo '<form action="seat_select.php" method="post" style="display:inline;">';
+    echo '<input type="hidden" name="time_slot" value="' . $slot . '">';
+    echo '<button class="time-button" type="submit">' . $slot . '</button>';
+    echo '</form>';
+}
+
+
+?>
+</div>
+
+<form action="top.php" method="get">
+    <button type="submit" class="back-button">戻る</button>
+</form>
+
+</body>
+</html>
